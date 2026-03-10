eff5d40f2c3a4d7e9679c1ca93167e35368336e7
lucasmendezdev <lucasmendezdev@gmail.com>
2026-03-10 11:19:51 -0300

    subo el proyecto

---
+++ js/data.js
@@ -0,0 +1,49 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Mate ranchero",
+    precio: 12000,
+    tipo: "mates",
+    imagen: "assets/img/mate1.jpeg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  {
+    id: 2,
+    nombre: "Mate torpedo",
+    precio: 12000,
+    tipo: "mates",
+    imagen: "assets/img/mate2.jpeg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  {
+    id: 3,
+    nombre: "Mate torpedo delux",
+    precio: 12000,
+    tipo: "mates",
+    imagen: "assets/img/mate3.jpeg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  {
+    id: 4,
+    nombre: "Termo acero",
+    precio: 12000,
+    tipo: "termos",
+    imagen: "assets/img/termo1.jpeg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  {
+    id: 5,
+    nombre: "Matera canasta cuero",
+    precio: 12000,
+    tipo: "canastas",
+    imagen: "assets/img/canasta1.jpeg",
+    talles: ["M", "L", "XL"],
+    descripcion: "Remera de algodón, cómoda y versátil para todos los días."
+  },
+  
+  
+];
